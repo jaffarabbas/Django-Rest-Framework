6695846aaec340553eb132821b34aa78db96282a
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-05 16:52:10 +0500

    05/02/2022 16:52:08

--- DRAFirstProject/api/views.py
+++ DRAFirstProject/api/views.py
@@ -1,3 +1,24 @@
+import json
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse,JsonResponse
 
-# Create your views here.
+# for single model      
+def Student_detail(request, pk):
+    stuDetail = Student.objects.get(id = pk)
+    serializers = StudentSerializer(stuDetail)
+    # json_data = JSONRenderer().render(serializers.data)
+    # return HttpResponse(json_data , content_type="application/json")
+    return JsonResponse(serializers.data)
+
+
+
+# for all data
+def Student_List(request):
+    stuDetail = Student.objects.all()
+    serializers = StudentSerializer(stuDetail, many=True)
+    # json_data = JSONRenderer().render(serializers.data)
+    # return HttpResponse(json_data , content_type="application/json")
+    return JsonResponse(serializers.data, safe=False)
