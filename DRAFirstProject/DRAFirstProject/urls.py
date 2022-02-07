fccb963455dc1fe622311a287fa13bc06583c5a2
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-07 21:59:08 +0500

    07/02/2022 21:59:06

--- DRAFirstProject/DRAFirstProject/urls.py
+++ DRAFirstProject/DRAFirstProject/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('stuinfo/<int:pk>', views.Student_detail),
     path('stuinfo/', views.Student_List),
+    path('stucreate/', views.Student_Create),
 ]
