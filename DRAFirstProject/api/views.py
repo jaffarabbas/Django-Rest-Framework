1ee5727741d834a07af85a5d8285ba469789fc88
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-07 21:59:08 +0500

    07/02/2022 21:59:06

--- DRAFirstProject/api/views.py
+++ DRAFirstProject/api/views.py
@@ -1,9 +1,12 @@
 import json
 from django.shortcuts import render
 from .models import Student
+import io
+from rest_framework.parsers import JSONParser
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # for single model      
 def Student_detail(request, pk):
@@ -20,3 +23,21 @@ def Student_List(request):
     # json_data = JSONRenderer().render(serializers.data)
     # return HttpResponse(json_data , content_type="application/json")
     return JsonResponse(serializers.data, safe=False)
+
+
+# Create your views here.
+@csrf_exempt
+def Student_Create(request):
+    if(request.method == 'POST'):
+        json_data = request.body
+        stream =  io.BytesIO(json_data)
+        data = JSONParser().parse(stream)
+        serializers = StudentSerializer(data=data) 
+        if serializers.is_valid():
+            serializers.save()
+            res = {'msg':'Data send'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data,content_type='application/json')
+
+        json_data = JSONRenderer().render(serializers.errors)
+        return HttpResponse(json_data,content_type='application/json')
