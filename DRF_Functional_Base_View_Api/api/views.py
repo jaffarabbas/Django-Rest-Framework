b43bc1b7221a85c7f77a6322a1d7989c633fa464
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-11 13:00:06 +0500

    11/02/2022 13:00:05

--- DRF_Functional_Base_View_Api/api/views.py
+++ DRF_Functional_Base_View_Api/api/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Create your views here.
-# @api_view()
-# def check(request):
-#     return Response({'msg': 'helo'}) 
+@api_view()
+def check(request):
+    return Response({'msg': 'helo'}) 
 # 
 # 
 #
 
-@api_view(["POST"])
-def check(request):
-    if request.method == 'POST':
-        print(request.body)
-        return Response({'msg': 'helo'})    
+# @api_view(["POST"])
+# def check(request):
+#     if request.method == 'POST':
+#         print(request.body)
+#         return Response({'msg': 'helo'})    
