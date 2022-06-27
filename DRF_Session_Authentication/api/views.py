640a0d94fc4e2d9b487466e5181beca13a84a73e
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-28 00:21:13 +0500

    Updated api/views.py at 28/06/2022 00:21:13

--- DRF_Session_Authentication/api/views.py
+++ DRF_Session_Authentication/api/views.py
@@ -2,7 +2,7 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated , AllowAny , IsAdminUser , IsAuthenticatedOrReadOnly , DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticated , AllowAny , IsAdminUser , IsAuthenticatedOrReadOnly , DjangoModelPermissions , DjangoModelPermissionsOrAnonReadOnly
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -12,4 +12,5 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticated] 
     # permission_classes = [AllowAny] 
     # permission_classes = [IsAdminUser] 
-    permission_classes = [DjangoModelPermissions]
+    # permission_classes = [DjangoModelPermissions]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
