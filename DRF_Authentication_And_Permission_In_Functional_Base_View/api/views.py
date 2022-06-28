b56825add984db63a611e272a2331ab234cad994
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-28 20:07:40 +0500

    Updated api/views.py at 28/06/2022 20:07:40

--- DRF_Authentication_And_Permission_In_Functional_Base_View/api/views.py
+++ DRF_Authentication_And_Permission_In_Functional_Base_View/api/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , authentication_classes , permission_classes
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 @api_view(['GET', 'POST', 'PUT', 'PATCH' ,'DELETE'])
-def student_api(request):
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_api(request,pk = None):
     if request.method == 'GET':
-        id = request.data.get('id')
+        # id = request.data.get('id')
+        id = pk
         if id is not None:
             stuData = Student.objects.get(id=id)
             serializers = StudentSerializer(stuData)
