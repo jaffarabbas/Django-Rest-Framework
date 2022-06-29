c8eafa30a2b5defd1810424cdd36ffe599077828
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-29 19:38:05 +0500

    Updated DRF_TokenAuthentication_ByUser/urls.py at 29/06/2022 19:38:05

--- DRF_TokenAuthentication_ByUser/DRF_TokenAuthentication_ByUser/urls.py
+++ DRF_TokenAuthentication_ByUser/DRF_TokenAuthentication_ByUser/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 
@@ -25,5 +26,6 @@ router.register('studentapi', views.StudentModelViewSet,basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls',namespace='rest_framework'))
+    path('auth/', include('rest_framework.urls',namespace='rest_framework')),
+    path('gettoken/', obtain_auth_token)
 ]
