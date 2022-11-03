8217a14091684156205832c0895d6436c2a8071d
jaffarabbas <gamaportal8@gmail.com>
2022-11-03 20:40:33 +0500

    Inserted EmployeesApi/api/views.py at 03/11/2022 20:40:33

---
+++ Projects/EmployeesApi/api/views.py
@@ -0,0 +1,7 @@
+from .models import Employee
+from .serializers import EmployeeSerializer
+from rest_framework import viewsets
+
+class EmployeeModelViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
